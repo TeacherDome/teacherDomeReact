5fae95a44da2b3f8d7de07f9a1c1659613e24929
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-11-27 14:02:21 -0500

    link update student to app js

---
+++ src/updateStudent.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import api from './api'
+
+class updateStudent extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+	return ( <section>	
+			<p>AAAHAHAHA</p>
+				<form action="/api/students/updateStudent" method="post">
+				
+				<lable> First Name:<input type="text" name="StudentFirstName" /></lable>
+				<lable> Last Name<input type="text" name="StudentLastName" /></lable>
+				<lable> Student School Id Num<input type="text" name="StudentSchoolIdNumber" /></lable>
+				<lable> Family Note<input type="text" name="StudentFamilyNotes" /></lable>
+				<lable> Helth Note <input type="text" name="StudentHealthNotes" /></lable>
+				<input type="submit" value="Submit" />
+				</form>
+			</section>
+		)
+	}
+}
+
+export default updateStudent
