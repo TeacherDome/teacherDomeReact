8ab35c9fdf0915153c783a3643c4c1dfa7ecba93
Riley <rileyjeanpatrick@gmail.com>
2018-11-27 15:14:47 -0500

    Form not working

--- src/AddStudent.js
+++ src/AddStudent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import api from './api'
+const addStudentButton = document.querySelector('button');
 
 class AddStudent extends Component {
 	constructor(props) {
@@ -9,16 +10,40 @@ class AddStudent extends Component {
 
 
 
+	addStudent = () =>{
+		addStudentButton.addEventListener('click', function() {
+		
+		const xhttp = new XMLHttpRequest()
+		
+		// Sets behavior for when the AJAX request is complete
+		xhttp.onreadystatechange = function() {
+			
+			// Checks the ready state and http status code
+			if (this.readyState == 4 && this.status == 200) {
+			}
+		}
+		xhttp.open('POST', `/api/students/addStudents`, true)
+		const body = JSON.stringify({
+					studentFirstName: this.studentFirstName.value,
+					studentLastName: this.studentLastName.value,
+					studentSchoolIdNumber: this.studentSchoolIdNumber.value
+				
+				})
+		xhttp.send(body)
+	})
+}
 
 	render() {
 		return (<section>
-				<form action="http://localhost:8080/api/students/addStudent" method="POST">
+
+		
+
 
 					<label>First Name: <input type="text" name="studentFirstName" placeholder= "Luke" required/></label>
 					<label>Last Name: <input type="text" name="studentLastName" placeholder= "Skywalker" required/></label>
 					<label>Student Id: <input type="text" name="studentSchoolIdNumber" placeholder ="Tatooine" required/></label>
-					<input type="submit" value="Submit" />
-				</form>
+					<button className="studentSubmit"  onClick={this.addStudent}>Submit</button>
+
 			</section>
 		)
 	}
