96470ffa92e4e90e5b5491877379b8d558e2c160
Riley <rileyjeanpatrick@gmail.com>
2018-11-28 11:10:11 -0500

    add student working

--- src/AddStudent.js
+++ src/AddStudent.js
@@ -10,28 +10,55 @@ class AddStudent extends Component {
 
 
 
-	addStudent = () =>{
-		addStudentButton.addEventListener('click', function() {
+// 	addStudent = () =>{
+// 		 addStudentButton.addEventListener('click', function() {
 		
-		const xhttp = new XMLHttpRequest()
+// 		const xhttp = new XMLHttpRequest()
 		
-		// Sets behavior for when the AJAX request is complete
-		xhttp.onreadystatechange = function() {
+// 		// Sets behavior for when the AJAX request is complete
+// 		xhttp.onreadystatechange = function() {
 			
-			// Checks the ready state and http status code
-			if (this.readyState == 4 && this.status == 200) {
-			}
-		}
-		xhttp.open('POST', `/api/students/addStudents`, true)
+// 			// Checks the ready state and http status code
+// 			if (this.readyState == 4 && this.status == 200) {
+// 			}
+// 		}
+// 		xhttp.open('POST', `/api/students/addStudents`, true)
+// 		const body = JSON.stringify({
+// 					studentFirstName: this.studentFirstName.value,
+// 					studentLastName: this.studentLastName.value,
+// 					studentSchoolIdNumber: this.studentSchoolIdNumber.value
+				
+// 				})
+// 		xhttp.send(body)
+// 	})
+// }
+addStudent(){
+	var firstName= document.getElementById("firstName").value;
+	var lastName= document.getElementById("lastName").value;
+	var schoolId= document.getElementById("schoolId").value;
+	var xhttp = new XMLHttpRequest()
+	var url = '/api/students/addStudent';
+// Sets behavior for when the AJAX request is complete
+xhttp.onreadystatechange = function() {
+	// Checks the ready state and http status code
+	if (this.readyState == 4 && this.status == 200) {
+		
+	}
+}
+
+	xhttp.open('POST', url, true)
 		const body = JSON.stringify({
-					studentFirstName: this.studentFirstName.value,
-					studentLastName: this.studentLastName.value,
-					studentSchoolIdNumber: this.studentSchoolIdNumber.value
+					studentFirstName: firstName,
+					studentLastName: lastName,
+					studentSchoolIdNumber: schoolId
+
 				
 				})
+		console.log(body)
 		xhttp.send(body)
-	})
+
 }
+
 
 	render() {
 		return (<section>
@@ -39,9 +66,9 @@ class AddStudent extends Component {
 		
 
 
-					<label>First Name: <input type="text" name="studentFirstName" placeholder= "Luke" required/></label>
-					<label>Last Name: <input type="text" name="studentLastName" placeholder= "Skywalker" required/></label>
-					<label>Student Id: <input type="text" name="studentSchoolIdNumber" placeholder ="Tatooine" required/></label>
+					<label>First Name: <input type="text" name="studentFirstName" id="firstName" placeholder= "Luke" required/></label>
+					<label>Last Name: <input type="text" name="studentLastName" id="lastName" placeholder= "Skywalker" required/></label>
+					<label>Student Id: <input type="text" name="studentSchoolIdNumber" id="schoolId" placeholder ="Tatooine" required/></label>
 					<button className="studentSubmit"  onClick={this.addStudent}>Submit</button>
 
 			</section>
