3908b6982a710e68bee9367d8bd9963dfb591064
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-12-04 15:06:28 -0500

    changes made to teach res page

---
+++ src/TeacherResourcePage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import api from './api'
+
+class TeacherResourcesPage  extends Component {
+	constructor(props) {
+		super(props) {
+		teacherResourceFolder: []
+		}
+	}
+
+render() {
+	
+		return (
+			<section className="folder-list">
+			{this.state.teacherResourceFolder.map((teacherResourceFolder, index) => (
+					<section className="teacherResourceFolderInList" key={index}>
+			)
+
+	}
+
+}
