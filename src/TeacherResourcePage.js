de9d108b764242655b0bf41387932d1f8d9656f3
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-12-05 11:19:16 -0500

    working teacher resoure page.

--- src/TeacherResourcePage.js
+++ src/TeacherResourcePage.js
@@ -3,20 +3,38 @@ import api from './api'
 
 class TeacherResourcesPage  extends Component {
 	constructor(props) {
-		super(props) {
-		teacherResourceFolder: []
+		super(props) 
+		this.state ={
+			teacherResourceFolders: []
 		}
 	}
+	
+	getTeacherResorce = () => {
+		api(`http://localhost:8080/api/TeacherResourcesPage`).then(teacherResourceFolders => {
+			const newFolder = this.state.teacherResourceFolders
+			teacherResourceFolders.forEach(teacherResourceFolder =>{
+				newFolder.push(teacherResourceFolder)
+		})
+			this.setState({ teacherResourceFolders: newFolder })
+	})
+}
+	componentDidMount() {
+		this.getTeacherResorce()
+	}
+
 
 render() {
 	
 		return (
 			<section className="folder-list">
-			{this.state.teacherResourceFolder.map((teacherResourceFolder, index) => (
-					<section className="teacherResourceFolderInList" key={index}>
-			)
-
-	}
-
+			<h2> Folder List </h2>
+			{this.state.teacherResourceFolders.map((teacherResourceFolder, index) => (
+					<section className="folderInList" key={index}>
+					</section>
+			))
+			}
+			</section>
+)
+}
 }
 export default TeacherResourcesPage
