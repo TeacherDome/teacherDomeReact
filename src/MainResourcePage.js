512d5296e660b8b1742268e0c4be4a8e16b52597
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-12-05 15:28:45 -0500

    resource page works on curent build

--- src/MainResourcePage.js
+++ src/MainResourcePage.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+import TeacherResourcePage from './TeacherResourcePage'
 
-const MainResourcePage = ({changeLocation}) => {
-	return (
-      <h1>This is the resource Page</h1>
-     
-   
+class MainStudentPage extends Component {
+	constructor() {
+		super()
+		this.state = {
+     teacherResourceFolders: []
+
+      	}
+	}
+
+
+	
+	render(){
+		return (
+		<section>
+		<div className= "MainResourcePage">
+    	  <h1>This is the resource Page</h1>
+   	
+    	 <TeacherResourcePage />
+    	 </div>
+   		</section>
 	)
+		
+	}
 }
-
-export default MainResourcePage
+export default MainStudentPage
