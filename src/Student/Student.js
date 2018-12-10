786bd722abc4872f714d90d75e9f05a0bba5a2d6
Riley <rileyjeanpatrick@gmail.com>
2018-12-10 13:46:14 -0500

    student info done

--- src/Student/Student.js
+++ src/Student/Student.js
@@ -99,12 +99,6 @@ class Student extends Component {
 				key={this.props.currentStudent.studentId}
 			>
 				<p className="name"> {this.props.currentStudent.studentFirstName} { " "}{this.props.currentStudent.studentLastName}</p>
-				<p>{"Student School ID: "}{this.props.currentStudent.studentSchoolIdNumber}</p>
-				<p>{"Student Date of Birth: "}{this.props.currentStudent.studentDateOfBirth}</p>
-				<p>{"Student Health Notes: "}{this.props.currentStudent.studentHealthNotes}</p>
-				<p>{"Student Progress Notes: "}{this.props.currentStudent.studentProgressNotes}</p>
-				<p>{"Student Comments : "}{this.props.currentStudent.studentComments}</p>
-
 
 				 <StudentOptions changeLocation={this.changeLocation} />
 				 <br/>
@@ -116,6 +110,7 @@ class Student extends Component {
 								studentId={this.props.currentStudent.studentId}
 								updateStudents={this.props.updateStudents}
 								checkCurrentStudent={this.checkCurrentStudent}
+								currentStudent={this.props.currentStudent}
 							/>
 
 							<br/>
--- src/Student/UpdateStudent.js
+++ src/Student/UpdateStudent.js
@@ -8,6 +8,10 @@ class UpdateStudent extends Component {
 			firstName: '',
 			lastName: '',
 			schoolId: '',
+			birthDate: '',
+			studentHealthNotes: '',
+			studentProgressNotes: '',
+			studentComments: '',
 			studentIdToUpdate: '',
 		
 		}
@@ -33,12 +37,31 @@ class UpdateStudent extends Component {
 		this.clearFields();
 	}
 	
+	onClickUpdateStudendBirthDate=()=>{
+		this.updateStudentDateofBirth();
+		this.clearFields();
+	}
 
-	
+	onClickUpdateStudendHealthNotes=()=>{
+		this.updateStudentHealthNotes();
+		this.clearFields();
+	}
+
+	onClickUpdateStudentProgress=()=>{
+		this.updateStudentProgress();
+		this.clearFields();
+	}
+
+	onClickUpdateStudendComments=()=>{
+		this.updateStudentComments();
+		this.clearFields();
+	}
+
 	clearFields=()=>{
 		this.state.firstName = ''
 		this.state.lastName = ''
 		this.state.schoolId = ''
+		this.state.birthDate = ''
 	}
 
 
@@ -54,6 +77,21 @@ class UpdateStudent extends Component {
 	updateSchoolId = event => {
 		this.setState({ schoolId: event.target.value })
 	}
+
+	updateBirthDate= event => {
+		this.setState({ birthDate: event.target.value })
+	}
+
+	updateHealthNotes= event => {
+		this.setState({ studentHealthNotes: event.target.value })
+	}
+	updateProgress= event => {
+		this.setState({ studentProgressNotes: event.target.value })
+	}
+	updateComments= event => {
+		this.setState({ studentComments: event.target.value })
+	}
+
 	updateStudentFirstName = () => {
 	 	var studentIdToUpdate = this.props.studentId;
 
@@ -71,7 +109,7 @@ class UpdateStudent extends Component {
 				this.props.checkCurrentStudent()
 			})
 	}
-		updateStudentLastName = () => {
+	updateStudentLastName = () => {
 	 	var studentIdToUpdate = this.props.studentId;
 
 		fetch('/api/students/updateStudentLastName', {
@@ -107,9 +145,92 @@ class UpdateStudent extends Component {
 				this.props.checkCurrentStudent()
 			})
 	}
+
+	updateStudentDateofBirth = () => {
+	 var studentIdToUpdate = this.props.studentId;
+
+		fetch('/api/students/updateStudentDateofBirth', {
+			method: 'PUT',
+			body: JSON.stringify({
+				studentDateOfBirth: this.state.birthDate,
+				studentId: studentIdToUpdate
+
+			})
+		})
+			.then(res => res.json())
+			.then(students => {
+				console.log("UpdateStudent.js", students)
+				this.updateAllStudents(students)
+				this.props.checkCurrentStudent()
+			})
+	}
+
+	updateStudentHealthNotes = () => {
+	 var studentIdToUpdate = this.props.studentId;
+
+		fetch('/api/students/updateStudentHealthNotes', {
+			method: 'PUT',
+			body: JSON.stringify({
+				studentHealthNotes: this.state.studentHealthNotes,
+				studentId: studentIdToUpdate
+
+			})
+		})
+			.then(res => res.json())
+			.then(students => {
+				console.log("UpdateStudent.js", students)
+				this.updateAllStudents(students)
+				this.props.checkCurrentStudent()
+			})
+	}
+
+	updateStudentProgress = () => {
+	 var studentIdToUpdate = this.props.studentId;
+
+		fetch('/api/students/updateStudentProgress', {
+			method: 'PUT',
+			body: JSON.stringify({
+				studentProgressNotes: this.state.studentProgressNotes,
+				studentId: studentIdToUpdate
+
+			})
+		})
+			.then(res => res.json())
+			.then(students => {
+				console.log("UpdateStudent.js", students)
+				this.updateAllStudents(students)
+				this.props.checkCurrentStudent()
+			})
+	}
+
+	updateStudentComments = () => {
+	 var studentIdToUpdate = this.props.studentId;
+
+		fetch('/api/students/updateStudentComments', {
+			method: 'PUT',
+			body: JSON.stringify({
+				studentComments: this.state.studentComments,
+				studentId: studentIdToUpdate
+
+			})
+		})
+			.then(res => res.json())
+			.then(students => {
+				console.log("UpdateStudent.js", students)
+				this.updateAllStudents(students)
+				this.props.checkCurrentStudent()
+			})
+	}
+
 	render() {
 		return (
 			<section className="update-student">
+			<p>{"Student School ID: "}{this.props.currentStudent.studentSchoolIdNumber}</p>
+				<p>{"Student Date of Birth: "}{this.props.currentStudent.studentDateOfBirth}</p>
+				<p>{"Student Health Notes: "}{this.props.currentStudent.studentHealthNotes}</p>
+				<p>{"Student Progress Notes: "}{this.props.currentStudent.studentProgressNotes}</p>
+				<p>{"Student Comments : "}{this.props.currentStudent.studentComments}</p>
+
 			<br/>
 			<h1 className="update-student-h1">Update Student Information</h1>
 			<br/>
@@ -158,6 +279,68 @@ class UpdateStudent extends Component {
 						onChange={this.updateSchoolId}
 					/>
 					<button className="studentSubmit" onClick={this.onClickUpdateStudentSchoolId}>Submit</button>
+				</label>
+
+
+				<br/>
+				<br/>
+				<label className="InputUpdate">
+					{' '}
+					Student Birth Date: 
+					<input
+						type="text"
+						name="birthDate"
+						id="birthDate"
+						value={this.state.birthDate}
+						onChange={this.updateBirthDate}
+					/>
+					<button className="studentSubmit" onClick={this.onClickUpdateStudendBirthDate}>Submit</button>
+				</label>
+
+				<br/>
+				<br/>
+				<label className="InputUpdate">
+					{' '}
+					Student Health Information: 
+					<input
+						type="text"
+						name="studentHealthNotes"
+						id="studentHealthNotes"
+						value={this.state.studentHealthNotes}
+						onChange={this.updateHealthNotes}
+					/>
+					<button className="studentSubmit" onClick={this.onClickUpdateStudendHealthNotes}>Submit</button>
+				</label>
+
+
+				<br/>
+				<br/>
+				<label className="InputUpdate">
+					{' '}
+					Student Progress Information: 
+					<input
+						type="text"
+						name="studentProgressNotes"
+						id="studentProgressNotes"
+						value={this.state.studentProgressNotes}
+						onChange={this.updateProgress}
+					/>
+					<button className="studentSubmit" onClick={this.onClickUpdateStudentProgress}>Submit</button>
+				</label>
+
+				<br/>
+				<br/>
+				<label className="InputUpdate">
+					{' '}
+					Student Progress Information: 
+					<input
+						type="text"
+						name="studentComments"
+						id="studentComments"
+						value={this.state.studentComments}
+						onChange={this.updateComments}
+					/>
+					<button className="studentSubmit" onClick={this.onClickUpdateStudendComments}>Submit</button>
 				</label>
 				</section>
 			</section>
