6963cfe6d74bf89b02ac48499feab13331ae7e27
Ingrid Biederman <biederman.14@gmail.com>
2018-12-10 10:08:48 -0500

    working on contacts

--- src/Student/Contact.js
+++ src/Student/Contact.js
@@ -1,24 +1,68 @@
 import React, { Component } from 'react'
+import api from '../api'
+import Contacts from './Contact'
+import AddContacts from './AddContacts'
+
 
 class Contact extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			contactFistName: '',
-			contactLastName: '',
-			contactStreet: '',
-			contactCity: '',
-			contactState: '',
-			conatctZipCode: '',
-			contactEmail; '',
-			contactCellPhoneNumber: '',
-			contactHomPhoneNumber: '',
-			contactWorkPhoneNumber: '',
-			contactPriority: ''
+			cctFistName: '',
+			cctLastName: '',
+			cctRelationship: '',
+			cctStreet: '',
+			cctCity: '',
+			cctState: '',
+			cctZipCode: '',
+			cctEmail; '',
+			cctCellPhoneNumber: '',
+			cctHomPhoneNumber: '',
+			cctWorkPhoneNumber: '',
+			cctPriority: ''
+		defaultContact: {
+			defaultCctFistName: '',
+			defaultCctLastName: '',
+			defaultCctRelationship: ''
+			}
 		}
 	}
 
 	updateAllContacts = response => {
 		this.props.updateContacts(response)
 	}
+
+	updateCurrentContact = response => {
+		this.props.changeContact(response)
+	}
+
+	defaultCurrentContact = () => {
+			this.props.changeContact(this.state.defaultContact)
+		}
+
+	checkCurrentContact = () => {
+		console.log("Here's your contact")
+		fetch('/api/contact', {
+			method: 'PUT',
+			body: JSON.stringify({
+				contactId: this.props.currentContact.contactId
+			})
+		})
+		.then(res => res.json())
+			.then(contacts => this.updateCurrentContact(contacts))
+	}
+
+render() {
+	return (
+		<section
+				id="contactSection"
+				key={this.props.currentContact.contactId}>
+				<p className="name"> {this.props.currentContact.contactFirstName} { " "}{this.props.currentStudent.ContactLastName}</p>		
+			)
+}
+
+
+}
+
+
 
