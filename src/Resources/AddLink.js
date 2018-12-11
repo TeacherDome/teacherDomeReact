bcf6ceb69adf701c46160eebc0a28fad6fde912f
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-12-11 13:55:54 -0500

    can add links

--- src/Resources/AddLink.js
+++ src/Resources/AddLink.js
@@ -7,44 +7,77 @@ class AddLink extends Component {
 		}
 	}
 
-onClick=()=>{
+	onClick= () => {
 		this.addLink();
 		this.clearFields();
 	}
 
-addLink = () => {
+	updateAllLinks= (response) => {
+		this.props.updateLinks(response)
+	}
+
+	updateLinkName = event => {
+		this.setState({ linkName: event.target.value })
+	}
+
+	updateLinkDescription = event => {
+		this.setState({linkDescription: event.target.value})
+	}
+
+	clearFields = () => {
+		this.state.linkName=''
+		this.state.linkDescription=''
+	}
+
+
+	addLink = () => {
 		fetch('/api/TeacherResourcePage/addLink', {
 			method: 'post',
 			body: JSON.stringify({
-				resourceLinkName: this.state.linkName,
+				LinkName: this.state.linkName,
+				LinkDescription: this.state.linkDescription,
+				FolderId: this.props.currentFolder.resourceFolderId
 			})
 		})
-			.then(res => res.json())
-			.then(teacherResourceFolders => this.updateAllFolders(teacherResourceFolders))
+		.then(res => res.json())
+		.then(links => this.updateAllLinks(links))
 	}
 
 	
 	render() {
 		return (
 			<section className="add-link">
-				<label>
-					Link Name:{' '}
-					<input
-						type="text"
-						name="teacherResourceLinkName"
-						id="linkName"
-						placeholder="home Work"
-						required
-						value={this.state.linkName}
-						onChange={this.updateLinkName}
-					/>
-				</label>
-				
-				
-				<button className="folderSubmit" onClick={this.onClick}>
-					Submit
-				</button>
+			<label>
+			Link Address:{' '}
+				<input
+				type="text"
+				name="teacherResourceLinkName"
+				id="linkName"
+				placeholder="somewhere"
+				required
+				value={this.state.linkName}
+				onChange={this.updateLinkName}
+				/>
+			</label>
+
+			<label>
+			Link Description:{''}
+				<input
+				type="text"
+				name="teacherResourceLinkDescription"
+				id="linkDescription"
+				required
+				value={this.state.linkDescription}
+				onChange={this.updateLinkDescription}
+				/>
+			</label>
+
+			<button className="linkSubmit" onClick={this.onClick}>
+				Submit
+			</button>
 			</section>
-		)
+			)
 	}
 }
+
+export default AddLink
